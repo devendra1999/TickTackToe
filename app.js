c192c01cba2fc72f1575bd8ab19b6fa138e487f0
Devendra Singh <devendrasamant8505@gmail.com>
2024-07-15 22:27:36 +0530

    fix: fixed the counter bug and code refactor

--- app.js
+++ app.js
@@ -1,74 +1,65 @@
-let boxes = document.querySelectorAll('.box');
-let resetBtn = document.querySelector('#reset-btn');
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
 
 let turn0 = true;
 let count = 0;
 
 const winPatterns = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 5, 8], 
-    [2, 4, 6],
-    [3, 4, 5], 
-    [6, 7, 8],
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
 ];
 
 boxes.forEach((box, index) => {
-    box.addEventListener('click', () => {
-        if (box.textContent === '' && turn0) {
-            box.textContent = 'X';
-            turn0 = false;
-            count++;
-        } else if(box.textContent === '' && !turn0) {
-            box.textContent = 'O';
-            turn0 = true;
-            count++;
-        }
-        box.disabled = true;
+  box.addEventListener("click", () => {
+    if (box.textContent === "" && turn0) {
+      box.textContent = "X";
+      turn0 = false;
+      count++;
+    } else if (box.textContent === "" && !turn0) {
+      box.textContent = "O";
+      turn0 = true;
+      count++;
+    }
+    box.disabled = true;
 
-        let winner =  checkWinCondition();
-        if(winner === true || winner === false) {
-        alert(`Player ${winner} wins!!!`)
-        resetBoard();
-        } else if (count === 9) {
-            alert('It\'s a draw!');
-            resetBoard();
-        }
-    });
-    
+    let winner = checkWinCondition();
+    if (winner) {
+      alert(`Player ${winner} wins!!!`);
+      resetBoard();
+    }
+  });
 });
 
-    const checkWinCondition = () => {
-        for (let pattern of winPatterns) {
-                let pos1Val = boxes[pattern[0]].innerText;
-                let pos2Val = boxes[pattern[1]].innerText;
-                let pos3Val = boxes[pattern[2]].innerText;
+const checkWinCondition = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
 
-                if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
-                    if (pos1Val === pos2Val && pos2Val === pos3Val){
-                    return pos1Val;
-                    }
-                }
-            }
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        return pos1Val;
+      }
+    }
+  }
+  if (count === 9) {
+    alert("It's a draw!");
+    resetBoard();
+  }
+};
 
-            // if (count === 9) {
-            //     alert('It\'s a draw!');
-            //     resetBoard();
-            // }
-    };
-    
-    const resetBoard = () => {
-        boxes.forEach((box) => {
-            box.textContent = '';
-            box.disabled = false;
-        });
-        turn0 = true;
-    };
-    
-    // resetBtn.addEventListener('click', resetBoard);
+const resetBoard = () => {
+  boxes.forEach((box) => {
+    box.textContent = "";
+    box.disabled = false;
+  });
+  turn0 = true;
+  count = 0;
+};
 
-
-    
-    
